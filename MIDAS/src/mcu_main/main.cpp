c98f5225ca4e5423697e8f1ebc76532aa50362c7
Zyun Lam <zyunlam2@illinois.edu>
2023-09-26 18:26:07 -0500

    Initial work on threads

--- MIDAS/src/mcu_main/main.cpp
+++ MIDAS/src/mcu_main/main.cpp
@@ -1,8 +1,19 @@
 #include <Arduino.h>
+#include <FreeRTOS.h>
+#include <task.h>
 
+static const int STACK_SIZE = 1024;
+void data_logger_thread(void* pvParameters) {
+    configASSERT(((uint32_t) pvParameters ) == 1);
+    while (true) {
+
+    }
+}
 
 void setup() {
-  
+    TaskHandle_t data_logger_task = NULL;
+    xTaskCreateStaticPinnedToCore(data_logger_thread, "data_logger", STACK_SIZE, NULL, tskIDLE_PRIORITY, &data_logger_task);
+    vTaskStartScheduler();
 }
 
 void loop() {
