c188011b8400c1f0fb77252e42b282f3617dcc2a
redindelible <redindelible@gmail.com>
2023-09-26 19:10:22 -0500

    Added sensors.h for the sensor object prototypes

---
+++ MIDAS/src/mcu_main/sensors.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/** This header provides the "prototypes" for the sensors, their public interfaces.
+ * That way, by switching out what .cpp files we compile in the platformio.ini,
+ * we can switch whether we're using hardware sensors or emulated sensors
+ */
+
+#include "mcu_main/sensor_data.h"
+
+
+struct LowGSensor {
+    LowGData read();
+};
