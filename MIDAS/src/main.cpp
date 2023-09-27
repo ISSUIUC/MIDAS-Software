0228093088e139c0516a7c8a5abdc6b27b8c9af5
redindelible <redindelible@gmail.com>
2023-09-27 12:00:34 -0500

    now with 50% more documentation per documentation
    Signed-off-by: redindelible <redindelible@gmail.com>

--- MIDAS/src/main.cpp
+++ MIDAS/src/main.cpp
@@ -2,8 +2,8 @@
 #include "sensors.h"
 
 /**
- * Sets the config file and then starts all the threads using the config 
-*/
+ * Sets the config file and then starts all the threads using the config.
+ */
 void setup() {
     RocketConfig config { .sensors = Sensors(), .rocket_state = RocketState() };
 
