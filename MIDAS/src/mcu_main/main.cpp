8b27461e7ecdced484d4de060945e6e03391d7b4
Zyun Lam <zyunlam2@illinois.edu>
2023-09-26 19:27:02 -0500

    Add threads

--- MIDAS/src/mcu_main/main.cpp
+++ MIDAS/src/mcu_main/main.cpp
@@ -2,20 +2,33 @@
 #include <FreeRTOS.h>
 #include <task.h>
 
-static const int STACK_SIZE = 1024;
-void data_logger_thread(void* pvParameters) {
-    configASSERT(((uint32_t) pvParameters ) == 1);
-    while (true) {
+static constexpr size_t STACK_SIZE = 1024;
 
+int i = 0;
+/* Task to be created. */
+void vTaskCode( void * pvParameters )
+{
+    /* The parameter value is expected to be 1 as 1 is passed in the
+    pvParameters value in the call to xTaskCreate() below. 
+    configASSERT( ( ( uint32_t ) pvParameters ) == 1 );*/
+    for( ;; )
+    {
+        /* Task code goes here. */
+        Serial.println("Pigfarts here I come!");
+        i++;
     }
 }
 
+void init_thread(TaskFunction_t function, const char* name, UBaseType_t priority, TaskHandle_t* handle) {
+    BaseType_t xReturned;
+    xReturned = xTaskCreate(function, name, STACK_SIZE, NULL, priority, handle);
+    if (xReturned != pdPASS) {
+        Serial.println("Failed to create task");
+    }
+}
 void setup() {
-    TaskHandle_t data_logger_task = NULL;
-    xTaskCreateStaticPinnedToCore(data_logger_thread, "data_logger", STACK_SIZE, NULL, tskIDLE_PRIORITY, &data_logger_task);
-    vTaskStartScheduler();
+    Serial.begin(9600);
 }
 
 void loop() {
-
 }
