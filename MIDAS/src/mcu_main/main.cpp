0153fbaabbff2f84101ebbf17311acfd274b765a
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-09-26 19:25:26 -0500

    Create sensor structs

--- MIDAS/src/mcu_main/main.cpp
+++ MIDAS/src/mcu_main/main.cpp
@@ -3,7 +3,17 @@
 #include <task.h>
 
 static const int STACK_SIZE = 1024;
+constexpr std::size_t SENSOR_CORE = 0;
+constexpr std::size_t DATA_CORE = 1;
+
 void data_logger_thread(void* pvParameters) {
+    configASSERT(((uint32_t) pvParameters ) == 1);
+    while (true) {
+
+    }
+}
+
+void altitude_thread(void* pvParameters) {
     configASSERT(((uint32_t) pvParameters ) == 1);
     while (true) {
 
@@ -13,6 +23,9 @@ void data_logger_thread(void* pvParameters) {
 void setup() {
     TaskHandle_t data_logger_task = NULL;
     xTaskCreateStaticPinnedToCore(data_logger_thread, "data_logger", STACK_SIZE, NULL, tskIDLE_PRIORITY, &data_logger_task);
+
+    TaskHandle_t altitude_task = NULL;
+    xTaskCreateStaticPinnedToCore(altitude_thread, "altitude", STACK_SIZE, NULL, tskIDLE_PRIORITY+1, &altitude_task);
     vTaskStartScheduler();
 }
 
--- MIDAS/src/mcu_main/sensor_data.h
+++ MIDAS/src/mcu_main/sensor_data.h
@@ -1,6 +1,90 @@
-#pragma once
+#pragma oncegit
 
 
 struct LowGData {
-    int gx, gy, gz;
+    float gx = 0;
+    float gy = 0;
+    float gz = 0;
 };
+
+struct HighGData {
+    float gx = 0;
+    float gy = 0;
+    float gz = 0;
+};
+
+struct Barometer {
+    float temperature = 0;
+    float pressure = 0;
+};
+
+struct Continuity {
+    bool is_continious = false;
+};
+
+struct Voltage {
+    float voltage = 0;
+};
+
+struct GPS {
+    float latitude = 0;
+    float longitudinal = 0;
+    float altitude = 0;
+    float satalite_count = 0;
+};
+
+struct Magnetometer {
+    float mx = 0;
+    float my = 0;
+    float mz = 0;
+};
+
+struct Orientation {
+    float yaw = 0;
+    float pitch = 0;
+    float roll = 0;
+
+    VelocityData orientation_velocity;
+    AccelerationData orientation_acceleration;
+
+    AccelerationData lienar_acceleration;
+
+    float gx = 0, gy = 0, gz = 0;
+
+    Magnetometer magnetometer;
+
+    float temperature;
+};
+
+struct PositionData {
+    float px = 0; 
+    float py = 0; 
+    float pz = 0;
+};
+
+struct VelocityData {
+    float vx = 0; 
+    float vy = 0; 
+    float vz = 0;
+};
+
+struct AccelerationData {
+    float ax =0 ; 
+    float ay = 0; 
+    float az = 0;
+};
+
+struct KalmanData {
+    PositionData position;
+    VelocityData velocity;
+    AccelerationData acceleration;
+
+    float altitude;
+
+    clock_t timeStamp_state = 0;
+};
+
+struct Pyro {
+    bool is_active = false;
+};
+
