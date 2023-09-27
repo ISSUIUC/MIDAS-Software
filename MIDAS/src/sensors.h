47e1cb31becaeb40515450d036e9b63f21120eac
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-09-27 14:20:12 -0500

    undo accidental push for sensors

--- MIDAS/src/sensors.h
+++ MIDAS/src/sensors.h
@@ -16,11 +16,4 @@ struct LowGSensor {
 
 struct Sensors {
     LowGSensor low_g;
-    HighGData high_g;
-    Barometer barometer;
-    Continuity continuity;
-    Voltage voltage;
-    GPS gps;
-    Magnetometer magnometer;
-    Orientation orientation;
 };
