8734207880f8a46eafacc466a1cb3ee53e9adf42
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2023-10-03 18:46:10 -0500

    more comments per comment

--- MIDAS/src/hardware/Barometer.cpp
+++ MIDAS/src/hardware/Barometer.cpp
@@ -1,24 +1,26 @@
 #include "sensors.h"
 #include <MS5611.h>
 
-// #include sensor library
+// using the sensor from https://registry.platformio.org/libraries/schm1tz1/MS5xxx/installation
+static MS5611 MS;
 
-// global static instance of the sensor
-MS5611 MS;
-
+/**
+ * Initializes barometer, returns NoError
+*/
 ErrorCode BarometerSensor::init() {
-    // do whatever steps to initialize the sensor
-    // if it errors, return the relevant error code
+    MS.init();
     return ErrorCode::NoError;
 }
 
+/**
+ * Reads the pressure and temperature from the MS5611
+ * @return a barometer data packet for the thread to send to the data logger
+*/
 Barometer BarometerSensor::read() const {
     MS.read(12);
 
     float pressure = static_cast<float>(MS.getPressure() * 0.01 + 26.03);
     float temperature = static_cast<float>(MS.getTemperature() * 0.01);
 
-    Barometer cur_state = {pressure, temperature};
-
-    return cur_state;
+    return Barometer{pressure, temperature};
 }
