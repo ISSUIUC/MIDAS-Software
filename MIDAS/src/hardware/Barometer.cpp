f63210035a83473f31ed9945c224edba4a3fae81
Nicholas Phillips <nwp2@illinois.edu>
2023-10-10 18:40:20 -0500

    add todo update

--- MIDAS/src/hardware/Barometer.cpp
+++ MIDAS/src/hardware/Barometer.cpp
@@ -24,6 +24,9 @@ ErrorCode BarometerSensor::init() {
 Barometer BarometerSensor::read() {
     MS.read(12);
 
+    /*
+     * TODO: Switch to latest version of library (0.3.9) when we get hardware to verify
+    */
     float pressure = static_cast<float>(MS.getPressure() * 0.01 + 26.03);
     float temperature = static_cast<float>(MS.getTemperature() * 0.01);
 
