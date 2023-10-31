524e4dcd3763cab6d19b94cd98e9282b0820316b
Ethan Pereira <ethanrp2@illinois.edu>
2023-10-31 18:47:45 -0500

    small changes

--- MIDAS/src/gnc/example_kf.cpp
+++ MIDAS/src/gnc/example_kf.cpp
@@ -7,7 +7,10 @@ void ExampleKalmanFilter::initialize() {
 
 }
 
-void ExampleKalmanFilter::priori() {}
+void ExampleKalmanFilter::priori() {
+    x_priori = (F_mat * x_k);
+    P_priori = (F_mat * P_k * F_mat.transpose()) + Q;
+}
 
 void ExampleKalmanFilter::update() {}
 
