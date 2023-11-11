2fd270bac0a268c6391066f8812f343005689ce5
Nicholas Phillips <nwp2@illinois.edu>
2023-11-11 17:18:55 -0600

    dummy sensors

--- MIDAS/src/hilsim/main.cpp
+++ MIDAS/src/hilsim/main.cpp
@@ -1,12 +1,15 @@
 #include <Arduino.h>
 #include <pb_decode.h>
 
-#include "hilsim/hilsimpacket.pb.h"
+#include <systems.h>
+#include "packet.h"
 
-void setup() {
-    pb_byte_t buffer[HILSIMPacket_size];
-    // Read the thing
-    // Serial???
+HILSIMPacket global_packet = HILSIMPacket_init_zero;
+pb_byte_t buffer[HILSIMPacket_size];
+
+RocketSystems systems;
+
+DECLARE_THREAD(hilsim, void*arg){
     while (true) {
         size_t size = Serial.readBytes(buffer, HILSIMPacket_size + 1);
         // Kill off null zeros?
@@ -15,12 +18,16 @@ void setup() {
         bool status = pb_decode(&stream, HILSIMPacket_fields, &packet);
         if (!status) {
             // Error
-            return;
         }
-        // Process information
-        // Loop
-        // Assign values to hilsim and do stuff
+        global_packet = packet;
+        THREAD_SLEEP(10);
     }
+}
+
+void setup() {
+    START_THREAD(hilsim, 1, nullptr);
+    begin_systems(&systems);
+    
 }
 
 void loop(){}
