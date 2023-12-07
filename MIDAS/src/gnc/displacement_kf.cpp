0543e582725c0408d1b07390d1217addd3328629
Rithvik <rbhogavilli@gmail.com>
2023-12-06 21:15:21 -0500

    Remove this->

--- MIDAS/src/gnc/displacement_kf.cpp
+++ MIDAS/src/gnc/displacement_kf.cpp
@@ -6,68 +6,68 @@ DisplacementKalmanFilter displacement_kf;
 
 DisplacementKalmanFilter::DisplacementKalmanFilter() : KalmanFilter()
 {
-    this->initialize();
+    initialize();
 }
 
 void DisplacementKalmanFilter::initialize()
 {
     // set initial transition matrix
-    this->setF(this->s_dt_);
+    setF(s_dt_);
     // set initial process noise matrix
-    this->setQ(this->s_dt_, this->spectral_density_);
+    setQ(s_dt_, spectral_density_);
 
     // barometer input for x displacement
-    this->H(0,0) = 1;
+    H(0,0) = 1;
     // accelerometer input for all accelerations
-    this->H(1, 2) = 1;
-    this->H(2, 5) = 1;
-    this->H(3, 8) = 1;
+    H(1, 2) = 1;
+    H(2, 5) = 1;
+    H(3, 8) = 1;
 
     // set measurement uncertainties
-    this->R(0, 0) = 2.0;
-    this->R(1, 1) = 1.9;
-    this->R(2, 2) = 10;
-    this->R(3, 3) = 10;
+    R(0, 0) = 2.0;
+    R(1, 1) = 1.9;
+    R(2, 2) = 10;
+    R(3, 3) = 10;
 }
 
 void DisplacementKalmanFilter::initialize(Barometer& barometerData, Acceleration& imuData)
 {
     // set initial state
-    this->x_k(0,0) = barometerData.altitude;
-    this->x_k(2,0) = imuData.ax;
-    this->x_k(5,0) = imuData.ay;
-    this->x_k(8,0) = imuData.az;
+    x_k(0,0) = barometerData.altitude;
+    x_k(2,0) = imuData.ax;
+    x_k(5,0) = imuData.ay;
+    x_k(8,0) = imuData.az;
 
-    this->initialize();
+    initialize();
 }
 
 void DisplacementKalmanFilter::priori()
 {
-    this->x_priori = (this->F_mat * this->x_k);
-    this->P_priori = (this->F_mat * this->P_k * this->F_mat.transpose()) + this->Q;
+    x_priori = (F_mat * x_k);
+    P_priori = (F_mat * P_k * F_mat.transpose()) + Q;
 }
 
 void DisplacementKalmanFilter::update()
 {
     Eigen::Matrix<float, 9, 9> identity = Eigen::Matrix<float, 9, 9>::Identity();
     
-    this->x_k = this->x_priori + this->K * (this->y_k - (this->H * this->x_priori));
-    P_k = (identity - this->K * this->H) * this->P_priori;
+    x_k = x_priori + K * (y_k - (H * x_priori));
+    P_k = (identity - K * H) * P_priori;
 }
 
 void DisplacementKalmanFilter::setQ(float dt, float sd)
 {
     for (int i = 0; i < 3; i++)
     {
-        this->Q(3 * i, 3 * i) = pow(dt, 5) / 20;
-        this->Q(3 * i, 3 * i + 1) = pow(dt, 4) / 8;
-        this->Q(3 * i, 3 * i + 2) = pow(dt, 3) / 6;
-        this->Q(3 * i + 1, 3 * i + 1) = pow(dt, 3) / 8;
-        this->Q(3 * i + 1, 3 * i + 2) = pow(dt, 2) / 2;
-        this->Q(3 * i + 2, 3 * i + 2) = dt;
-        this->Q(1, 0) = this->Q(0, 1);
-        this->Q(2, 0) = this->Q(0, 2);
-        this->Q(2, 1) = this->Q(1, 2);
+        Q(3 * i, 3 * i) = pow(dt, 5) / 20;
+        Q(3 * i, 3 * i + 1) = pow(dt, 4) / 8;
+        Q(3 * i, 3 * i + 2) = pow(dt, 3) / 6;
+        Q(3 * i + 1, 3 * i + 1) = pow(dt, 3) / 8;
+        Q(3 * i + 1, 3 * i + 2) = pow(dt, 2) / 2;
+        Q(3 * i + 2, 3 * i + 2) = dt;
+        Q(1, 0) = this->Q(0, 1);
+        Q(2, 0) = this->Q(0, 2);
+        Q(2, 1) = this->Q(1, 2);
     }
 
     Q *= sd;
@@ -77,12 +77,12 @@ void DisplacementKalmanFilter::setF(float dt)
 {
     for (int i = 0; i < 3; i++)
     {
-        this->F_mat(3 * i, 3 * i) = 1;
-        this->F_mat(3 * i, 3 * i + 1) = dt;
-        this->F_mat(3 * i, 3 * i + 2) = pow(dt, 2) / 2;
-        this->F_mat(3 * i + 1, 3 * i + 1) = 1;
-        this->F_mat(3 * i + 1, 3 * i + 2) = dt;
-        this->F_mat(3 * i + 2, 3 * i + 2) = 1;
+        F_mat(3 * i, 3 * i) = 1;
+        F_mat(3 * i, 3 * i + 1) = dt;
+        F_mat(3 * i, 3 * i + 2) = pow(dt, 2) / 2;
+        F_mat(3 * i + 1, 3 * i + 1) = 1;
+        F_mat(3 * i + 1, 3 * i + 2) = dt;
+        F_mat(3 * i + 2, 3 * i + 2) = 1;
     }
 }
 
@@ -90,48 +90,48 @@ KalmanData DisplacementKalmanFilter::getState()
 {
     KalmanData state;
     
-    state.position.px = this->x_k(0, 0);
-    state.position.py = this->x_k(3, 0);
-    state.position.pz = this->x_k(6, 0);
+    state.position.px = x_k(0, 0);
+    state.position.py = x_k(3, 0);
+    state.position.pz = x_k(6, 0);
 
-    state.velocity.vx = this->x_k(1, 0);
-    state.velocity.vy = this->x_k(4, 0);
-    state.velocity.vz = this->x_k(7, 0);
+    state.velocity.vx = x_k(1, 0);
+    state.velocity.vy = x_k(4, 0);
+    state.velocity.vz = x_k(7, 0);
 
-    state.acceleration.ax = this->x_k(2, 0);
-    state.acceleration.ay = this->x_k(5, 0);
-    state.acceleration.az = this->x_k(8, 0);
+    state.acceleration.ax = x_k(2, 0);
+    state.acceleration.ay = x_k(5, 0);
+    state.acceleration.az = x_k(8, 0);
 
     return state;
 }
 
 void DisplacementKalmanFilter::setState(KalmanData state)
 {
-    this->x_k(0, 0) = state.position.px;
-    this->x_k(3, 0) = state.position.py;
-    this->x_k(6, 0) = state.position.pz;
+    x_k(0, 0) = state.position.px;
+    x_k(3, 0) = state.position.py;
+    x_k(6, 0) = state.position.pz;
 
-    this->x_k(1, 0) = state.velocity.vx;
-    this->x_k(4, 0) = state.velocity.vy;
-    this->x_k(7, 0) = state.velocity.vz;
+    x_k(1, 0) = state.velocity.vx;
+    x_k(4, 0) = state.velocity.vy;
+    x_k(7, 0) = state.velocity.vz;
 
-    this->x_k(2, 0) = state.acceleration.ax;
-    this->x_k(5, 0) = state.acceleration.ay;
-    this->x_k(8, 0) = state.acceleration.az;
+    x_k(2, 0) = state.acceleration.ax;
+    x_k(5, 0) = state.acceleration.ay;
+    x_k(8, 0) = state.acceleration.az;
 }
 
 void DisplacementKalmanFilter::kfTick(float dt, float sd, Barometer& barometerData, Acceleration& imuData)
 {
-    this->setF(dt);
-    this->setQ(dt, sd);
-    this->priori();
+    setF(dt);
+    setQ(dt, sd);
+    priori();
 
-    this->y_k(0, 0) = barometerData.altitude;
-    this->y_k(1, 0) = imuData.ax;
-    this->y_k(2, 0) = imuData.ay;
-    this->y_k(3, 0) = imuData.az;
+    y_k(0, 0) = barometerData.altitude;
+    y_k(1, 0) = imuData.ax;
+    y_k(2, 0) = imuData.ay;
+    y_k(3, 0) = imuData.az;
 
-    this->update();
+    update();
 
     // data needs to get normalized after filtering
     // don't subtract normal acceleration from gravity before filtering
