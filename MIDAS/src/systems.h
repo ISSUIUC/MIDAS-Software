d68d2bdddc580e4222089a9c3599ab9c702c1ba4
redindelible <redindelible@gmail.com>
2024-02-13 14:45:39 -0600

    compiles under silsim

--- MIDAS/src/systems.h
+++ MIDAS/src/systems.h
@@ -4,14 +4,11 @@
 #include "sensors.h"
 #include "rocket_state.h"
 #include "data_logging.h"
-#include "hardware/Emmc.h"
-#include "hardware/SDLog.h"
 
 struct RocketSystems {
     Sensors sensors;
     RocketData rocket_data;
-    LogSink log_sink;
-    EmmcLog emmc_sink;
+    LogSink& log_sink;
     BuzzerController buzzer;
 };
 
