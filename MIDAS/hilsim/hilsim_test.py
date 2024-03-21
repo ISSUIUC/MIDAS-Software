c317e6f3c7c1b71c07e01aa404770b7f72bea5fe
Zyun Lam <zyunlam2@illinois.edu>
2024-03-20 22:03:32 -0500

    Fix hilsim?

--- MIDAS/hilsim/hilsim_test.py
+++ MIDAS/hilsim/hilsim_test.py
@@ -80,6 +80,53 @@ def csv_to_protobuf(parsed_csv) -> hilsimpacket_pb2.HILSIMPacket:
     hilsim_packet.ornt_temp = 0
     return hilsim_packet
 
+
+tmp = 0
+def new_protobuf() -> hilsimpacket_pb2.HILSIMPacket:
+    global tmp
+    hilsim_packet = hilsimpacket_pb2.HILSIMPacket()
+    tmp = tmp + 1
+    hilsim_packet.imu_high_ax = tmp
+    hilsim_packet.imu_high_ay = tmp
+    hilsim_packet.imu_high_az = tmp
+    hilsim_packet.barometer_altitude = tmp
+    hilsim_packet.barometer_temperature = tmp
+    hilsim_packet.barometer_pressure = tmp
+    hilsim_packet.imu_low_lsm_ax = tmp
+    hilsim_packet.imu_low_lsm_ay = tmp
+    hilsim_packet.imu_low_lsm_az = tmp
+    hilsim_packet.imu_low_lsm_gx = tmp
+    hilsim_packet.imu_low_lsm_gy = tmp
+    hilsim_packet.imu_low_lsm_gz = tmp
+    hilsim_packet.mag_x = tmp
+    hilsim_packet.mag_y = tmp
+    hilsim_packet.mag_z = tmp
+
+    hilsim_packet.imu_low_ax = tmp
+    hilsim_packet.imu_low_ay = tmp
+    hilsim_packet.imu_low_az = tmp
+    hilsim_packet.ornt_roll = tmp
+    hilsim_packet.ornt_pitch = tmp
+    hilsim_packet.ornt_yaw = tmp
+    hilsim_packet.ornt_rollv = tmp
+    hilsim_packet.ornt_pitchv = tmp
+    hilsim_packet.ornt_yawv = tmp
+    hilsim_packet.ornt_rolla = tmp
+    hilsim_packet.ornt_pitcha = tmp
+    hilsim_packet.ornt_yawa = tmp
+    hilsim_packet.ornt_ax = tmp
+    hilsim_packet.ornt_ay = tmp
+    hilsim_packet.ornt_az = tmp
+    hilsim_packet.ornt_gx = tmp
+    hilsim_packet.ornt_gy = tmp
+    hilsim_packet.ornt_gz = tmp
+    hilsim_packet.ornt_mx = tmp
+    hilsim_packet.ornt_my = tmp
+    hilsim_packet.ornt_mz = tmp
+    hilsim_packet.ornt_temp = tmp
+    return hilsim_packet
+
+
 # Get first serial port...
 if __name__ == "__main__":
     MIDAS = serial_ports()
@@ -89,28 +136,32 @@ if __name__ == "__main__":
 
     MIDAS.write(b'!')
     print("Reading")
-    MIDAS.flush()
+
     # Read input
     print(MIDAS.read_until())
     print(MIDAS.read_until())
     print(MIDAS.read_until())
     print(MIDAS.read_until())
-    while True:
-        print(MIDAS.read_until())
+
     # Read the csv
-    csv = pandas.read_csv(os.path.dirname(os.path.abspath(sys.argv[0])) + "/flight_computer.csv", index_col=0)
-    for index, line in csv.iterrows():
-        packet = csv_to_protobuf(line)
+    csv = pandas.read_csv(os.path.dirname(os.path.abspath(sys.argv[tmp])) + "/flight_computer.csv", index_col=0)
+    while True:
+        packet = new_protobuf()
         data = packet.SerializeToString()
         # Encode the length of package in 2 bytes and then output the the information
-        MIDAS.write([len(data)])
-        print("writing {} bytes".format(len(data)))
+
+        MIDAS.write(len(data).to_bytes(2, "big"))
+        byte_len = len(data).to_bytes(2, "big")
+        # print(int.from_bytes(byte_len))
         MIDAS.write(data)
-        time.sleep(0.01)
-        size = int.from_bytes(MIDAS.read())
-        r = MIDAS.read(size)
-        state = rocketstate_pb2.RocketState()
-        state.ParseFromString(r)
-        print(state.rocket_state)
         #output = MIDAS.read_all()
-        #print(output)
+        print(MIDAS.read_all())
+        # print("Dumping data...")
+        by = MIDAS.read()
+        data2 = MIDAS.read(int.from_bytes(by))
+        state = rocketstate_pb2.RocketState()
+        state.ParseFromString(data2)
+        print(state.rocket_state)
+        #print(len(data).to_bytes(2, "big"))
+        #print(data)
+        # print("More stuff")
