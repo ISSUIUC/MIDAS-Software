cdcda9c207687836d4357f6cc2655676cd54bba8
Aadityavoru <av34@illinois.edu>
2024-04-23 18:33:36 -0500

    fixed to new telem packet

--- ground/src/feather/main.cpp
+++ ground/src/feather/main.cpp
@@ -39,7 +39,7 @@
 // #define LED 13 // Blinks on receipt
 
 // Change to 434.0 or other frequency, must match RX's freq!
-#define RF95_FREQ 104.0
+#define RF95_FREQ 434.0
 
 #define DEFAULT_CMD 0
 #define MAX_CMD_LEN 10
@@ -130,7 +130,7 @@ void EnqueuePacket(const TelemetryPacket& packet, float frequency) {
 
     int64_t start_printing = millis();
 
-    FullTelemetryData data{};
+    FullTelemetryData data;
     data.timestamp = start_printing;
     data.barometer_altitude = packet.alt;
     data.latitude = convert_range(packet.lat, 180);
@@ -140,6 +140,8 @@ void EnqueuePacket(const TelemetryPacket& packet, float frequency) {
     data.highG_az = convert_range(packet.highg_az, 32);
     data.battery_voltage = convert_range(packet.batt_volt, 5);
     data.FSM_State = packet.fsm_satcount;
+    print_queue.emplace(data);
+
 }
 
 void printJSONField(const char* name, float val, bool comma = true) {
@@ -169,7 +171,6 @@ void printJSONField(const char* name, const char* val, bool comma = true) {
 
 void printPacketJson(FullTelemetryData const& packet) {
     Serial.print(R"({"type": "data", "value": {)");
-    printJSONField("timestamp", packet.timestamp);
     printJSONField("barometer_altitude", packet.barometer_altitude);
     printJSONField("latitude", packet.latitude);
     printJSONField("longitude", packet.longitude);
@@ -201,12 +202,10 @@ void set_freq_local_bug_fix(float freq) {
 }
 
 void SerialInput(const char* key, const char* value) {
-    /* If queue is not empty, do not accept new command*/
     if (!cmd_queue.empty()) {
         Serial.println(json_buffer_full_error);
         return;
     }
-
     telemetry_command command{};
     if (strcmp(key, "ABORT") == 0) {
         command.command = CommandType::ABORT;
@@ -242,141 +241,57 @@ void SerialInput(const char* key, const char* value) {
 
 void process_command_queue() {
     if (cmd_queue.empty()) return;
-
     TelemetryCommandQueueElement cmd = cmd_queue.front();
     rf95.send((uint8_t*)&cmd.command, sizeof(cmd.command));
     rf95.waitPacketSent();
 }
 
 SerialParser serial_parser(SerialInput, SerialError);
-
 void setup() {
     while (!Serial)
         ;
     Serial.begin(9600);
-
     if (!rf95.init()) {
         Serial.println(json_init_failure);
         while (1)
             ;
     }
-
     pinMode(LED_BUILTIN, OUTPUT);
     Serial.println(json_init_success);
-
-    // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM
     if (!rf95.setFrequency(RF95_FREQ)) {
         Serial.println(json_set_frequency_failure);
         while (1)
             ;
     }
-
     Serial.print(R"({"type": "freq_success", "frequency":)");
     Serial.print(RF95_FREQ);
     Serial.println("}");
-
-    // Defaults after init are 434.0MHz, 13dBm, Bw = 125 kHz, Cr = 4/5, Sf =
-    // 128chips/symbol, CRC on
-
-    // The default transmitter power is 13dBm, using PA_BOOST.
-    // If you are using RFM95/96/97/98 modules which uses the PA_BOOST
-    // transmitter pin, then you can set transmitter powers from 5 to 23 dBm:
     rf95.setTxPower(23, false);
 }
 
 void loop() {
+    
     PrintDequeue();
-    if (Serial.available()) {
-        String input = Serial.readStringUntil('\n');
-        input.trim(); // Remove leading/trailing whitespace
-
-        // Check if input is a frequency value
-        float frequency = input.toFloat();
-        if (frequency >= 390 && frequency <= 445) { // Check if within valid range
-            // Set the LoRa frequency
-            rf95.setFrequency(frequency);
-            current_freq = frequency;
-
-            // Send success message over serial
-            Serial.print(R"({"type": "freq_success", "frequency":)");
-            Serial.print(frequency);
-            Serial.println("}");
-        } else {
-            // Send error message over serial for invalid frequency
-            Serial.println(R"({"type": "freq_error", "error": "invalid frequency"})");
-        }
-    }
-    // static float f = 0;
-    // static float f2 = 0;
-    // f+=0.1;
-    // f2 += 0.01;
-    // if(f > 3.14) f -= 6.28;
-    // delay(30);
-    // FullTelemetryData d{};
-    // d.barometer_pressure = 1000;
-    // d.barometer_temp = 20;
-    // d.bno_pitch = cos(f2);
-    // d.bno_roll = sin(f2);
-    // d.bno_yaw = 0;
-    // d.flap_extension = f / 10;
-    // d.freq = 434;
-    // d.FSM_State = 3;
-    // d.gnc_state_ax = f * 100;
-    // d.gnc_state_vx = f * 10;
-    // d.gnc_state_x = f * 1000;
-    // d.gnc_state_apo = 100;
-    // d.gps_alt = 1000+100*f;
-    // d.gps_lat = 40;
-    // d.gps_long = 80 + f;
-    // d.gyro_x = sin(f2);
-    // d.gyro_y = f+30;
-    // d.gyro_z = f+40;
-    // d.highG_ax = 10+f;
-    // d.highG_ay = f/10;
-    // d.highG_az = f/10;
-    // d.mag_x = f;
-    // d.mag_y = f+1;
-    // d.mag_z = f+2;
-    // d.voltage_battery = f + 4;
-    // printPacketJson(d);
-
     if (rf95.available()) {
-        // Should be a message for us now
         uint8_t buf[RH_RF95_MAX_MESSAGE_LEN];
-        // telemetry_data data{};
         TelemetryPacket packet;
         uint8_t len = sizeof(buf);
 
         if (rf95.recv(buf, &len)) {
-            // Serial.println(len);
-            // Serial.println("Received packet");
-            // Serial.println(packet.datapoints[0].barometer_pressure);
-            
             digitalWrite(LED_BUILTIN, HIGH);
             delay(50);
             digitalWrite(LED_BUILTIN, LOW);
+            Serial.println("Received packet");
+            Serial.println(len);
             memcpy(&packet, buf, sizeof(packet));
             EnqueuePacket(packet, current_freq);
-            // Serial.print("Queue size: ");
-            // Serial.println((print_queue.size()));
-
             if (!cmd_queue.empty()) {
                 auto& cmd = cmd_queue.front();
-                // if (cmd.command.id == packet.response_ID) {
-                //     if (cmd.command.command == CommandType::SET_FREQ) {
-                //         set_freq_local_bug_fix(cmd.command.freq);
-                //         Serial.print(R"({"type": "freq_success", "frequency":)");
-                //         Serial.print(cmd.command.freq);
-                //         Serial.println("}");
-                //     }
-                //     cmd_queue.pop();
-                // } else {
                     cmd.retry_count++;
                     if (cmd.retry_count >= max_command_retries) {
                         cmd_queue.pop();
                         Serial.println(json_send_failure);
                     }
-                // }
             }
 
             process_command_queue();
