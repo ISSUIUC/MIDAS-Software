877e6d72c3f57adf720f2192f86e3dc1bcd145ef
Nicholas Phillips <31660017+NPIPHI@users.noreply.github.com>
2024-04-27 16:53:22 -0500

    compliance update (#50)
    * compliance update
    
    * compile fix

--- MIDAS/src/telemetry_packet.h
+++ MIDAS/src/telemetry_packet.h
@@ -12,22 +12,11 @@ struct TelemetryPacket {
     uint16_t highg_ay; //14 bit signed ax [-16,16) 2 bit tilt angle
     uint16_t highg_az; //14 bit signed ax [-16,16) 2 bit tilt angle
     uint8_t batt_volt;
-    uint8_t fsm_satcount;
-};
-
-// Commands transmitted from ground station to rocket
-enum CommandType {
-    EMPTY = 0,
-    SET_FREQ,
-    SET_CALLSIGN
-};
-
-struct telemetry_command {
-    CommandType command;
-    int cmd_id;
-    union {
-        char callsign[8];
-        float freq;
-    };
-    std::array<char, 6> verify;
+    
+    
+    // If callsign bit (highest bit of fsm_callsign_satcount) is set, the callsign is KD9ZMJ
+    //
+    // If callsign bit (highest bit of fsm_callsign_satcount) is not set, the callsign is KD9ZPM
+    
+    uint8_t fsm_callsign_satcount; //4 bit fsm state, 1 bit is_sustainer_callsign, 3 bits sat count
 };
