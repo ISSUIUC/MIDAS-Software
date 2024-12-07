774a71841ceeccfd6d93b6af052a2fe8dc439ded
Aditya Srikanth <as140@illinois.edu>
2024-12-07 01:23:42 -0600

    went over kalman_filter.h, nothing crazy

--- MIDAS/src/gnc/kalman_filter.h
+++ MIDAS/src/gnc/kalman_filter.h
@@ -7,10 +7,10 @@
 
 #include <Eigen/Eigen>
 #include "sensor_data.h"
-#include "systems.h"
+#include "systems.h" //ignore
 
 
-struct KalmanState {
+struct KalmanState {    // class that keeps track of pos, vel, accel in xyz coordinates
     float state_est_pos_x;
     float state_est_vel_x;
     float state_est_accel_x;
@@ -26,22 +26,22 @@ template <int _NumStates, int _NumInputs>
 class KalmanFilter
 {
 protected:
-    Eigen::Matrix<float, _NumStates, 1> x_k;
-    Eigen::Matrix<float, _NumStates, _NumStates> F_mat;
-    Eigen::Matrix<float, _NumInputs, _NumStates> H;
-    Eigen::Matrix<float, _NumStates, _NumStates> P_k;
-    Eigen::Matrix<float, _NumStates, _NumStates> Q;
-    Eigen::Matrix<float, _NumInputs, _NumInputs> R;
-    Eigen::Matrix<float, _NumStates, _NumStates> P_priori;
-    Eigen::Matrix<float, _NumStates, 1> x_priori;
-    Eigen::Matrix<float, _NumStates, _NumInputs> K;
-    Eigen::Matrix<float, _NumInputs, 1> y_k;
+    Eigen::Matrix<float, _NumStates, 1> x_k; // State Vector: current estimated state of system
+    Eigen::Matrix<float, _NumStates, _NumStates> F_mat; // State Transition Matrix: shows how system evolves from one state to another
+    Eigen::Matrix<float, _NumInputs, _NumStates> H; // Measurement matrix: relates state variables to observed measurements
+    Eigen::Matrix<float, _NumStates, _NumStates> P_k; // Error Covariance Matrix : represents uncertainty in measurements at current step
+    Eigen::Matrix<float, _NumStates, _NumStates> Q; // Process Noise Covariance : represents uncertainty introduced by model 
+    Eigen::Matrix<float, _NumInputs, _NumInputs> R; // Measurement Noise Covariance : represents uncertainty in measurements (sensor noise)
+    Eigen::Matrix<float, _NumStates, _NumStates> P_priori; // Priori Error Covariance : predicted uncertainty of state before measurement
+    Eigen::Matrix<float, _NumStates, 1> x_priori; // Priori State Estimate : predicted state of system before measurements considered
+    Eigen::Matrix<float, _NumStates, _NumInputs> K; // Kalman Gain : figures weight given to new measurements vs predictions during update phase
+    Eigen::Matrix<float, _NumInputs, 1> y_k; // Measurement Residual : difference between predicted and actual measurements
 
-    Eigen::Matrix<float, _NumStates, _NumInputs> B;
+    Eigen::Matrix<float, _NumStates, _NumInputs> B; // Control Matrix
 
 public:
     KalmanFilter()
-    {
+    {   // initializes everything to zero (who would have guessed....)
         x_k = Eigen::Matrix<float, _NumStates, 1>::Zero();
         F_mat = Eigen::Matrix<float, _NumStates, _NumStates>::Zero();
         H = Eigen::Matrix<float, _NumInputs, _NumStates>::Zero();
@@ -55,7 +55,7 @@ public:
 
         B = Eigen::Matrix<float, _NumStates, _NumInputs>::Zero();
     }
-    
+    // same here
     virtual void initialize(RocketSystems* args) = 0;
     virtual void priori() = 0;
     virtual void update(Barometer barometer, Acceleration acceleration, Orientation orientation, FSMState current_state) = 0;
