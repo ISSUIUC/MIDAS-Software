6c0eeeada7d1ea46e270744198536dce2cc91e80
mpkarpov-ui <mkarpov2@illinois.edu>
2025-01-09 10:39:15 -0600

    intermediate work

--- MIDAS/src/hardware/Orientation.cpp
+++ MIDAS/src/hardware/Orientation.cpp
@@ -25,6 +25,13 @@ ErrorCode OrientationSensor::init() {
     return ErrorCode::NoError;
 }
 
+
+
+float angle_between_quaternions(const Quaternion& q1, const Quaternion& q2) {
+    float dot_product = Quaternion::dot(q1, q2);
+    return 2.0f * std::acos(std::fabs(dot_product));
+}
+
 /**
  * @brief Turns a quaternion into its corresponding Euler 3D vector representation
  * 
@@ -76,6 +83,25 @@ Vec3 quaternionToEulerGI(sh2_GyroIntegratedRV_t* rotational_vector, bool degrees
                              degrees);
 }
 
+std::tuple<float, float, float> euler_to_vector(float pitch, float yaw) {
+    float x = cos(pitch) * cos(yaw);
+    float y = cos(pitch) * sin(yaw);
+    float z = sin(pitch);
+    return {x, y, z};
+}
+
+float angular_difference(float pitch1, float yaw1, float pitch2, float yaw2) {
+    auto [x1, y1, z1] = euler_to_vector(pitch1, yaw1);
+    auto [x2, y2, z2] = euler_to_vector(pitch2, yaw2);
+
+    float mag1 = sqrt(x1 * x1 + y1 * y1 + z1 * z1);
+    float mag2 = sqrt(x2 * x2 + y2 * y2 + z2 * z2);
+
+    float dot_product = x1 * x2 + y1 * y2 + z1 * z2;
+
+    return std::acos(dot_product/(mag1*mag2));
+}
+
 /**
  * @brief Reads and returns the data from the sensor
  * 
@@ -85,13 +111,23 @@ Orientation OrientationSensor::read() {
     // read from aforementioned global instance of sensor
     sh2_SensorValue_t event;
     Vec3 euler;
+    Quaternion quat;
     if (imu.getSensorEvent(&event)) {
         switch (event.sensorId) {
             case SH2_ARVR_STABILIZED_RV:
                 euler = quaternionToEulerRV(&event.un.arvrStabilizedRV, true);
+                quat.w = event.un.arvrStabilizedRV.real;
+                quat.x = event.un.arvrStabilizedRV.i;
+                quat.y = event.un.arvrStabilizedRV.j;
+                quat.z = event.un.arvrStabilizedRV.k;
+                break;
             case SH2_GYRO_INTEGRATED_RV:
                 // faster (more noise?)
                 euler = quaternionToEulerGI(&event.un.gyroIntegratedRV, true);
+                quat.w = event.un.gyroIntegratedRV.real;
+                quat.x = event.un.gyroIntegratedRV.i;
+                quat.y = event.un.gyroIntegratedRV.j;
+                quat.z = event.un.gyroIntegratedRV.k;
                 break;
         }
 
@@ -119,15 +155,19 @@ Orientation OrientationSensor::read() {
 
         if (initial_flag == 0) {
             initial_orientation = sensor_reading;
+            initial_quaternion = quat;
             initial_flag = 1;
         }
 
         // calculate tilt from initial orientation
-        Orientation deviation;
-        deviation.yaw = min(abs(sensor_reading.yaw - initial_orientation.yaw), 2 * 3.14F - abs(sensor_reading.yaw - initial_orientation.yaw));
-        deviation.pitch = min(abs(sensor_reading.pitch - initial_orientation.pitch), 2 * 3.14F - abs(sensor_reading.pitch - initial_orientation.pitch));
+        // Orientation deviation;
+        // deviation.yaw = min(abs(sensor_reading.yaw - initial_orientation.yaw), 2 * 3.14F - abs(sensor_reading.yaw - initial_orientation.yaw));
+        // deviation.pitch = min(abs(sensor_reading.pitch - initial_orientation.pitch), 2 * 3.14F - abs(sensor_reading.pitch - initial_orientation.pitch));
+        // sensor_reading.tilt = sqrt(pow(deviation.yaw, 2) + pow(deviation.pitch, 2));
 
-        sensor_reading.tilt = sqrt(pow(deviation.yaw, 2) + pow(deviation.pitch, 2));
+        // sensor_reading.tilt = angular_difference(sensor_reading.pitch, sensor_reading.yaw, initial_orientation.pitch, initial_orientation.yaw);
+
+        sensor_reading.tilt = angle_between_quaternions(quat, initial_quaternion);
 
         return sensor_reading;
     }
