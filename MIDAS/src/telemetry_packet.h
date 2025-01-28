62b78f65e4fd65a16ed49b66be2a8cac58d661df
Zyun Lam <zyunlam2@illinois.edu>
2025-01-27 23:29:24 -0600

    Add more telem attributes

--- MIDAS/src/telemetry_packet.h
+++ MIDAS/src/telemetry_packet.h
@@ -18,13 +18,14 @@ struct TelemetryPacket {
     uint16_t highg_az; //14 bit signed ax [-16,16) 2 bit tilt angle
     uint8_t batt_volt;
     
-    
     // If callsign bit (highest bit of fsm_callsign_satcount) is set, the callsign is KD9ZMJ
     //
     // If callsign bit (highest bit of fsm_callsign_satcount) is not set, the callsign is KD9ZPM
     
     uint8_t fsm_callsign_satcount; //4 bit fsm state, 1 bit is_sustainer_callsign, 3 bits sat count
-};
+    uint16_t kf_x; // 16 bit meters
+    uint32_t pyro; // 7 bit continuity 4 bit tilt
+}  __attribute__((packed));
 
 
 // Commands transmitted from ground station to rocket
