eebe89a1b138bedd523342353f9a899cf0cdd7c3
SuragNuthulapaty <77648585+SuragNuthulapaty@users.noreply.github.com>
2025-01-31 19:46:40 -0600

    Python FSM Simulator (#62)
    * FSM testing system
    
    * remove csv
    
    * implement booster simulator
    
    * state machine argument
    
    * Gitignored
    
    * remove the csv
    
    * plots

---
+++ MIDAS/src/finite-state-machines/tester/fsm_test.py
@@ -0,0 +1,62 @@
+import argparse
+import pathlib
+import pandas as pd
+import fsm
+import math
+import matplotlib.pyplot as plt
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("filename", type=pathlib.Path)
+parser.add_argument("-s", "--stage", help="boost/sus", default="sus")
+
+args = parser.parse_args()
+
+state_machine = fsm.SustainerFSM()
+
+if args.stage == "boost":
+    state_machine = fsm.BoosterFsm()
+
+df = pd.read_csv(args.filename)
+packet = fsm.StateEstimate()
+
+state_rows = []
+
+for i, line in df.iterrows():
+    packet['acceleration'] = float(line['accel_x'])
+    packet['altitude'] = float(line['pos_x'])
+    packet['current_time'] = float(line['time'])
+    packet['jerk'] = 0
+
+    if i > 0 and line['time'] - df['time'][i - 1] != 0:
+        packet['jerk'] = float((line['accel_x'] - df['accel_x'][i - 1]) / (line['time'] - df['time'][i - 1]))
+
+    packet['vertical_speed'] = float(line['vel_x'])
+
+    state, transition_reason = state_machine.tick_fsm(packet)
+
+    if transition_reason != "":
+        print("\t".join([fsm.FSM_STATE_TO_STRING[state], transition_reason, str(state.value)]))
+    
+    state_rows.append(state.value)
+
+top_state = max(state_rows)
+
+df = pd.read_csv(args.filename)
+
+yticks = [fsm.FSM_STATE_TO_STRING[s] for s in fsm.FSMState]
+ynums = [i for i in range(14)]
+
+plt.yticks(ynums, yticks)
+plt.xlabel("Time (s)")
+plt.plot(df["time"], state_rows)
+
+plt.vlines(60, 0, top_state, label="Booster Ignition", colors="r", linestyles='--')
+plt.vlines(64, 0, top_state, label="Booster Burnout", colors="b", linestyles='--')
+plt.vlines(65, 0, top_state, label="Sustainer Ignition", colors="g", linestyles='--')
+plt.vlines(70, 0, top_state, label="Sustainer Burnout", colors="c", linestyles='--')
+
+
+plt.legend()
+plt.show()
+
