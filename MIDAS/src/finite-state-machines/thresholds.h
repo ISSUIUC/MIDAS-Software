b45d83ecd719d9488b3868b54278a93abe801398
Michael K <mkarpov2@illinois.edu>
2025-01-31 23:40:40 -0600

    pls

--- MIDAS/src/finite-state-machines/thresholds.h
+++ MIDAS/src/finite-state-machines/thresholds.h
@@ -11,6 +11,9 @@
 // SECOND STAGE THRESHOLDS
 // ----------------------------------
 
+// Regardless of sensor inputs, stay on pyro firing states for at LEAST this time.
+#define sustainer_pyro_firing_time_minimum 200
+
 // Transition to FIRST_BOOST if acceleration is greater than this
 #define sustainer_idle_to_first_boost_acceleration_threshold 3
 
@@ -18,7 +21,7 @@
 #define sustainer_idle_to_first_boost_time_threshold 1000
 
 // Transition to SECOND_BOOST from SUSTAINER_IGNITION if acceleration greater than this
-#define sustainer_ignition_to_second_boost_acceleration_threshold 3
+#define sustainer_ignition_to_second_boost_acceleration_threshold 4
 
 // Return state to SECOND_BOOST if not boosting for this amount of time (ms)
 #define sustainer_second_boost_to_coast_time_threshold 1000
@@ -45,7 +48,7 @@
 #define sustainer_main_to_main_deploy_timer_threshold 3000
 
 // Height required to deploy the main parachutes
-#define sustainer_main_deploy_altitude_threshold 3000
+#define sustainer_main_deploy_altitude_threshold 1006
 
 // Return to SUSTAINER_IGNITION if not in SECOND_BOOST for this amount of time (ms)
 #define sustainer_ignition_to_second_boost_time_threshold 1000
@@ -60,7 +63,13 @@
 #define sustainer_first_boost_to_burnout_time_threshold 1000
 
 // Transition to LANDED from MAIN if vertical speed is less than this threshold
-#define sustainer_landed_vertical_speed_threshold 20
+#define sustainer_landed_vertical_speed_threshold 3
+
+// Lock out further transitions from LANDED after this much time passes in the LANDED state. (ms)
+#define sustainer_landed_time_lockout 60000
+
+// Prevent us from inadvertently entering the LANDED state when we're at a low velocity at main deploy.
+#define sustainer_main_to_landed_lockout 5000
 
 // Stores a small jerk value
 #define sustainer_drogue_jerk_threshold 200
@@ -72,6 +81,9 @@
 // ----------------------------------
 // FIRST STAGE THRESHOLDS
 // ----------------------------------
+
+// Regardless of sensor inputs, stay on pyro firing states for at LEAST this time.
+#define booster_pyro_firing_time_minimum 200
 
 // Transition to FIRST_BOOST if acceleration is greater than this
 #define booster_idle_to_first_boost_acceleration_threshold 3
@@ -101,7 +113,10 @@
 #define booster_main_to_main_deploy_timer_threshold 3000
 
 // Height required to deploy the main parachutes
-#define booster_main_deploy_altitude_threshold 3000
+
+// [STARGAZER 1.4] This is a "dontcare" value --> The booster does not have a drogue, we transition immediately to MAIN,
+#define booster_main_deploy_altitude_threshold 999999
+
 // Return to SUSTAINER_IGNITION if not in SECOND_BOOST for this amount of time (ms)
 #define booster_ignition_to_second_boost_time_threshold 1000
 
@@ -115,7 +130,13 @@
 #define booster_first_boost_to_burnout_time_threshold 1000
 
 // Transition to LANDED from MAIN if vertical speed is less than this threshold
-#define booster_landed_vertical_speed_threshold 20
+#define booster_landed_vertical_speed_threshold 4
+
+// Lock out further transitions from LANDED after this much time passes in the LANDED state. (ms)
+#define booster_landed_time_lockout 60000
+
+// Prevent us from inadvertently entering the LANDED state when we're at a low velocity at main deploy.
+#define booster_main_to_landed_lockout 5000
 
 // Stores a small jerk value
 #define booster_first_separation_jerk_threshold 300
