dbff31d162aaf279d332c4911b95b979ff2352d0
Nicholas Phillips <nwp2@illinois.edu>
2025-03-08 20:04:26 -0600

    Ground Station Copied

---
+++ ground/src/feather_duo/Packet.cpp
@@ -0,0 +1,58 @@
+#include<Arduino.h>
+#include"Packet.h"
+
+
+int decodeLastTwoBits(uint16_t ax, uint16_t ay, uint16_t az) {
+    int tilt_ax = ax & 0b11;
+    int tilt_ay = ay & 0b11;
+    int tilt_az = az & 0b11;
+    int tilt = (tilt_ax << 0) | (tilt_ay << 2) | (tilt_az << 4);
+    return tilt;
+}
+
+double ConvertGPS(int32_t coord) {
+    double complete = static_cast<double>(coord) / 10000000.0;
+    return complete;
+}
+
+template <typename T>
+float convert_range(T val, float range) {
+    size_t numeric_range = (int64_t)std::numeric_limits<T>::max() - (int64_t)std::numeric_limits<T>::min() + 1;
+    return val * range / (float)numeric_range;
+}
+
+FullTelemetryData DecodePacket(const TelemetryPacket& packet, float frequency) {
+    int64_t start_printing = millis();
+    FullTelemetryData data;
+    data.altitude = static_cast<float>(packet.alt);
+    data.latitude = ConvertGPS(packet.lat);
+    data.longitude = ConvertGPS(packet.lon);
+    data.barometer_altitude = convert_range<int16_t>(packet.baro_alt, 1 << 17);
+    int tilt = decodeLastTwoBits(packet.highg_ax, packet.highg_ay, packet.highg_az);
+    tilt |= (packet.pyro >> 28 & (0xF)) << 6;
+    int16_t ax = packet.highg_ax & 0xfffc;
+    int16_t ay = packet.highg_ay & 0xfffc;
+    int16_t az = packet.highg_az & 0xfffc;
+    data.highG_ax = convert_range<int16_t>(ax, 32);
+    data.highG_ay = convert_range<int16_t>(ay, 32);
+    data.highG_az = convert_range<int16_t>(az, 32);
+    data.tilt_angle = tilt / 1023. * 180; // Returns tilt angle in range [0, 180]
+    data.battery_voltage = convert_range(packet.batt_volt, 16);
+    data.sat_count = packet.fsm_callsign_satcount >> 4 & 0b0111;
+    data.is_sustainer = (packet.fsm_callsign_satcount >> 7);
+    data.FSM_State = packet.fsm_callsign_satcount & 0b1111;
+    data.pyros[0] = (packet.pyro >> 0) & (0x7F);
+    data.pyros[1] = ((float) ((packet.pyro >> 7) & (0x7F)) / 127.) * 12.;
+    data.pyros[2] = ((float) ((packet.pyro >> 14) & (0x7F)) / 127.) * 12.;
+    data.pyros[3] = ((float) ((packet.pyro >> 21) & (0x7F)) / 127.) * 12.;
+    data.kf_reset = packet.alt & 1;
+
+    // kinda hacky but it will work
+    if (packet.fsm_callsign_satcount == static_cast<uint8_t>(-1)) {
+        data.FSM_State = static_cast<uint8_t>(-1);
+    }
+    data.kf_vx = (float) packet.kf_vx / (float) ((1 << 16) - 1) * 4000.f - 2000.f;
+    data.freq = frequency;
+
+    return data;
+}
---
+++ ground/src/feather_duo/main.cpp
@@ -0,0 +1,199 @@
+#include<Arduino.h>
+#include<SPI.h>
+#include"FreeRTOS.h"
+#include"E22.h"
+#include"duo_pins.h"
+#include"Packet.h"
+#include"Output.h"
+#include"Command.h"
+#include"Queue.h"
+
+constexpr uint32_t BOOSTER_FREQ = 425150000;
+constexpr uint32_t SUSTAINER_FREQ = 426150000;
+Queue<TelemetryCommand> booster_cmds;
+Queue<TelemetryCommand> sustainer_cmds;
+
+enum class Stage {
+    Booster,
+    Sustainer
+};
+
+struct RadioConfig
+{
+    SX1268 * radio;
+    Queue<TelemetryCommand>* cmd_queue;
+    uint32_t frequnecy;
+    Stage stage;
+    uint8_t indicator_led;
+};
+
+
+bool init_radio(SX1268& radio, uint32_t frequency) {
+    if(radio.setup() != SX1268Error::NoError) return false;
+    if(radio.set_frequency(frequency) != SX1268Error::NoError) return false;
+    if(radio.set_modulation_params(8, LORA_BW_250, LORA_CR_4_8, false) != SX1268Error::NoError) return false;
+    if(radio.set_tx_power(22) != SX1268Error::NoError) return false;
+
+    return true;
+}
+
+void Radio_Rx_Thread(void * arg) {
+    RadioConfig* cfg = (RadioConfig*)arg;
+    bool led_state = false;
+    bool reset_state = false;
+    TelemetryCommand to_send;
+    to_send.command = CommandType::EMPTY;
+
+    while(true) {
+        TelemetryPacket packet{};
+        SX1268Error res = cfg->radio->recv((uint8_t*)&packet, sizeof(packet), -1);
+        if(res == SX1268Error::NoError) {
+            led_state = !led_state;
+            digitalWrite(cfg->indicator_led, led_state);
+            FullTelemetryData data = DecodePacket(packet, cfg->frequnecy / 1e6);
+            data.rssi = cfg->radio->get_last_snr();
+            printPacketJson(data);
+
+            if(data.kf_reset != reset_state) {
+                data.kf_reset = reset_state;
+                to_send.command = CommandType::EMPTY;
+            }
+
+            if(to_send.command == CommandType::EMPTY) {
+                if(!cfg->cmd_queue->receive(&to_send)){
+                    to_send.command = CommandType::EMPTY;
+                }
+            }
+
+            if(to_send.command != CommandType::EMPTY) {
+                (void)cfg->radio->send((uint8_t*)&to_send, sizeof(to_send));
+            }
+        }
+    }
+}
+
+void handle_serial(const String& key) {
+    TelemetryCommand command{};
+
+    if(key.length() < 1) {
+        Serial.println(json_command_bad);
+        return;
+    }
+    Stage stage;
+    switch(key[0]) {
+        case '0':
+            stage = Stage::Booster;
+            break;
+        case '1':
+            stage = Stage::Sustainer;
+            break;
+        default:
+            Serial.println(json_command_bad);
+            return;
+    }
+
+    String cmd_name = key.substring(1);
+
+    if (cmd_name == "RESET_KF") {
+        command.command = CommandType::RESET_KF;
+    } else if (cmd_name == "SAFE") {
+        command.command = CommandType::SWITCH_TO_SAFE;
+    } else if (cmd_name == "IDLE") {
+        command.command = CommandType::SWITCH_TO_IDLE;
+    } else if (cmd_name == "PT") {
+        command.command = CommandType::SWITCH_TO_PYRO_TEST;
+    } else if (cmd_name == "PA") {
+        command.command = CommandType::FIRE_PYRO_A;
+    } else if (cmd_name == "PB") {
+        command.command = CommandType::FIRE_PYRO_B;
+    } else if (cmd_name == "PC") {
+        command.command = CommandType::FIRE_PYRO_C;
+    } else if (cmd_name == "PD") {
+        command.command = CommandType::FIRE_PYRO_D;
+    } else if (cmd_name == "CAMT") {
+        command.command = CommandType::CAM_TOGGLE;
+    } else {
+        Serial.println(json_command_bad);
+        return;
+    }
+
+    if(stage == Stage::Booster) {
+        booster_cmds.send(command);
+    }
+    if(stage == Stage::Sustainer) {
+        sustainer_cmds.send(command);
+    }
+
+    Serial.println(json_command_success);
+}
+
+void Management_Thread(void * arg) {
+    String cur_input = "";
+    bool led_state = false;
+    while(true){
+        while(Serial.available()) {
+            char input = Serial.read();
+            if(input == '\n') {
+                cur_input.replace("\r", "");
+                handle_serial(cur_input);
+                cur_input = "";
+            } else {
+                cur_input += input;
+            }
+        }
+        led_state = !led_state;
+        digitalWrite(Pins::LED_GREEN, led_state);
+        delay(10); 
+    }
+}
+
+void setup() {
+    Serial.begin(460800);
+    
+    SPIClass SPI0(HSPI);
+    SPIClass SPI1(FSPI);
+    SX1268 Radio0(SPI0, Pins::E22_CS_0, Pins::E22_BUSY_0, Pins::E22_DIO1_0, Pins::E22_RXEN_0, Pins::E22_TXEN_0, Pins::E22_RESET_0);
+    SX1268 Radio1(SPI1, Pins::E22_CS_1, Pins::E22_BUSY_1, Pins::E22_DIO1_1, Pins::E22_RXEN_1, Pins::E22_TXEN_1, Pins::E22_RESET_1);
+    pinMode(Pins::LED_BLUE, OUTPUT);
+    pinMode(Pins::LED_GREEN, OUTPUT);
+    pinMode(Pins::LED_ORANGE, OUTPUT);
+    pinMode(Pins::LED_RED, OUTPUT);
+    digitalWrite(Pins::LED_RED, HIGH);
+
+    SPI0.begin(Pins::SPI_SCK_0, Pins::SPI_MISO_0, Pins::SPI_MOSI_0);
+    SPI1.begin(Pins::SPI_SCK_1, Pins::SPI_MISO_1, Pins::SPI_MOSI_1);
+    
+    if(!init_radio(Radio0, BOOSTER_FREQ)) Serial.println(json_init_failure);
+    if(!init_radio(Radio1, SUSTAINER_FREQ)) Serial.println(json_init_failure);
+    Serial.println(json_init_success);
+    digitalWrite(Pins::LED_RED, LOW);
+    digitalWrite(Pins::LED_GREEN, HIGH);
+
+
+    RadioConfig booster_cfg{
+        .radio=&Radio0,
+        .cmd_queue=&booster_cmds,
+        .frequnecy=BOOSTER_FREQ,
+        .stage=Stage::Booster,
+        .indicator_led=Pins::LED_ORANGE,
+    };
+
+    RadioConfig sustainer_cfg{
+        .radio=&Radio1,
+        .cmd_queue=&sustainer_cmds,
+        .frequnecy=SUSTAINER_FREQ,
+        .stage=Stage::Sustainer,
+        .indicator_led=Pins::LED_BLUE,
+    };
+
+    xTaskCreate(Radio_Rx_Thread, "Radio0_thread", 8192, &booster_cfg, 0, nullptr);
+    xTaskCreate(Radio_Rx_Thread, "Radio1_thread", 8192, &sustainer_cfg, 0, nullptr);
+    xTaskCreate(Management_Thread, "Managmenet_thread", 8192, nullptr, 0, nullptr);
+    while(true) {
+        delay(10000);
+    }
+}
+
+void loop() {
+    delay(10000);
+}
