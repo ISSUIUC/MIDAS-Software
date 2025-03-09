346bcbf4c1e82fd25ff4e8cad78106bc79b449a0
Michael K <mkarpov2@illinois.edu>
2025-03-09 05:00:58 -0500

    add kf stuff

--- MIDAS/src/telemetry.cpp
+++ MIDAS/src/telemetry.cpp
@@ -105,14 +105,14 @@ TelemetryPacket Telemetry::makePacket(RocketData& data) {
     // Pack all data into pyro struct (we will eventually use pyro properly, just not now.)
     // Roll rate
     constexpr float max_roll_rate_hz = 10.0f;
+    constexpr float max_kf_altitude = 35000.0f;
     float roll_rate_hz = std::clamp(std::abs(orientation.angular_velocity.vx) / (2.0f*static_cast<float>(PI)), 0.0f, max_roll_rate_hz);
-
+    float kf_px_clamped = std::clamp(kalman.position.px, 0.0f, max_kf_altitude);
     const float max_volts = 12;
 
-    packet.pyro |= ((((uint16_t) (roll_rate_hz / max_roll_rate_hz * 255)) & 0xFF) << (0 * 8)); // bits 0-7
-    packet.pyro |= ((((uint16_t) (data.camera_state)) & 0xFF) << (1 * 8));                     // bits 8-15
-    // packet.pyro |= ((((uint16_t) (continuity.pins[2] / max_volts * 127)) & 0x7F) << (2 * 7)); unused (for now)
-    packet.pyro |= ((((uint16_t) (continuity.pins[3] / max_volts * 127)) & 0x7F) << (2 * 8));
+    packet.pyro |= ((((uint16_t) (roll_rate_hz / max_roll_rate_hz * 255)) & 0xFF) << (0 * 8));    // bits 0-7
+    packet.pyro |= ((((uint16_t) (data.camera_state)) & 0xFF) << (1 * 8));                        // bits 8-15
+    packet.pyro |= ((((uint16_t) (kf_px_clamped / max_kf_altitude * 4095)) & 0xFFF) << (2 * 8));  // bits 16-27
     packet.pyro |= tilt_extra << 28;
 
     static_assert(FSMState::FSM_STATE_COUNT < 16);
