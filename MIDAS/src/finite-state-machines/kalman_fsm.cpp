14c1751ebb1697940acec8c4911808e545f03da4
Ishaan Kandamuri <ishaan.kandamuri@gmail.com>
2025-04-14 19:58:09 -0500

    WIP, populated kalman_fsm.h but we should consider switching variable types if necessary. KalmanFSM.cpp in TARS-Software has a lot of dependencies which don't clearly align with MIDAS so we have to run through them all and make adjustments to corresponding fns

--- MIDAS/src/finite-state-machines/kalman_fsm.cpp
+++ MIDAS/src/finite-state-machines/kalman_fsm.cpp
@@ -1,3 +1,21 @@
 #include <cmath>
 
 #include "kalman_fsm.h"
+
+double KalmanFSM::getAltitudeAverage(size_t start, size_t len) {
+
+}
+
+double KalmanFSM::getSecondDerivativeAltitudeAverage(size_t start, size_t len) {
+
+}
+
+double KalmanFSM::getAccelerationAverage(size_t start, size_t len) {
+
+}
+
+void KalmanFSM::tickFSM() {
+    
+
+
+}
---
+++ MIDAS/src/finite-state-machines/kalman_fsm.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "fsm_states.h"
+#include "fsm.h"
+
+class KalmanFSM : public FSM  {
+   public:
+    KalmanFSM() = default;
+
+    void tickFSM() override;
+
+    private:
+    systime_t launch_time_ = 0;
+    sysinterval_t burn_timer_ = 0;
+    systime_t burnout_time_ = 0;
+    sysinterval_t coast_timer_ = 0;
+
+    systime_t apogee_time_ = 0;
+    sysinterval_t apogee_timer_ = 0;
+
+    systime_t drogue_time_ = 0;
+    sysinterval_t drogue_timer_ = 0;
+
+    systime_t main_time_ = 0;
+    sysinterval_t main_timer_ = 0;
+
+    systime_t landing_time_ = 0;
+    sysinterval_t landing_timer = 0;
+
+    double getAltitudeAverage(size_t start, size_t len);
+    double getSecondDerivativeAltitudeAverage(size_t start, size_t len);
+    double getAccelerationAverage(size_t start, size_t len);
+
+};
