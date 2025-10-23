9b6c974e2b7b426e54902c9ffbadb6c3b8869776
Divij <diviaops@gmail.com>
2025-10-22 19:55:23 -0500

    Merged GNC final into Main

---
+++ MIDAS/src/gnc/constants.h
@@ -0,0 +1,10 @@
+// constants
+const float pi = 3.14159268;
+const float a = 343.0;                // (m/s) speed of sound
+const float rho = 1.225;              // average air density
+const float r = 0.0396;              // (m)
+const float height_full = 3.0259;      // (m) height of rocket Full Stage
+const float height_sustainer = 1.5021; // (m) height of rocket Sustainer
+const float mass_full = 10.6;        // (kg) Sustainer + Booster
+const float mass_sustainer = 4.68;   // (kg) Sustainer
+const float gravity_ms2 = 9.81;           // (m/s^2) accel due to gravity
