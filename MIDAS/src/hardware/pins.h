1e2c8a9c3d6875d43d1362a784918984c1919ccc
ishrakj <ishrakj2@illinois.edu>
2026-02-03 17:34:24 -0600

    updated pins in pins.h

--- MIDAS/src/hardware/pins.h
+++ MIDAS/src/hardware/pins.h
@@ -1,95 +1,79 @@
 #pragma once
 
 // SPI sensor bus
-#define SPI_MISO 39
-#define SPI_MOSI 37
-#define SPI_SCK 38
+#define SPI_MISO 3
+#define SPI_MOSI 2
+#define SPI_SCK 1
 
 // barometer chip select
-#define MS5611_CS 43
-
-// gyro chip select
-#define LSM6DS3_CS 36
-
-// high g chip select 
-#define KX134_CS 40
-
-// low g chip select
-#define ADXL355_CS 44
+#define MS5611_CS 44
 
 // magnetometer chip select
-#define LIS3MDL_CS 35
+#define MAG_CS 4
+#define MAG_INT 7
 
-// orientation chip select, interrupt
-#define BNO086_CS 48
-#define BNO086_INT 34
-#define BNO086_RESET 33
-
-// voltage adc pin
-#define VOLTAGE_PIN 0
-
-// gps pins
-#define GNSS_I2C_LOCATION 0x3A
-#define GPS_RESET GpioAddress(2, 017)
-#define GPS_ENABLE 0
+// IMU chip select, interrupt
+#define LSM_INT1 15
+#define LSM_INT2 42
+#define LSM_CS 43
 
 // i2c bus pins
-#define I2C_SDA 10
-#define I2C_SCL 9
-
-#define PYRO_SDA 41
-#define PYRO_SCL 42
+#define I2C_SDA 21
+#define I2C_SCL 26
 
 // Buzzer pins
-#define BUZZER_PIN 17
+#define BUZZER_PIN 12
 #define BUZZER_CHANNEL 1
 
-// CAN pins
-#define CAN_CS 2
-#define RFM96W_CS 5
+// GPIO Expander pins
+#define EXP_RESET 13
+#define EXP_INT 48
 
-// GPS I2C location
-#define GNSS_I2C_LOCATION 0x3A
-#define GPS_RESET GpioAddress(2, 017)
-#define GPS_ENABLE 0
-
-//take the pins that are in base 8 and update it to the right base - this is not done yet.
+// GPS pins
+#define SAM_RESET 5
 
 // pyro pins
-#define PYRO_GLOBAL_ARM_PIN GpioAddress(0, 05)
-#define PYROA_FIRE_PIN GpioAddress(0, 04)
-#define PYROB_FIRE_PIN GpioAddress(0, 03)
-#define PYROC_FIRE_PIN GpioAddress(0, 01)
-#define PYROD_FIRE_PIN GpioAddress(0, 00)
+#define PYRO_GLOBAL_ARM_PIN GpioAddress(0, 3)
+#define PYROA_FIRE_PIN GpioAddress(0, 0)
+#define PYROB_FIRE_PIN GpioAddress(0, 1)
+#define PYROC_FIRE_PIN GpioAddress(0, 7)
+#define PYROD_FIRE_PIN GpioAddress(0, 6)
+#define PYRO_PG GpioAddress(0, 4)
 
-// Continuity Pins
-#define SENSE_APOGEE 010
-#define SENSE_MAIN 011
-#define SENSE_MOTOR 06
-#define SENSE_AUX 07
+// Continuity pins (on the ADC)
+#define SENSE_A 0
+#define SENSE_B 1
+#define SENSE_C 4
+#define SENSE_D 5
 
-// Telemetry pins
-#define RFM96_INT 7 // DEPRECTED NO LONGER USE
+// Voltage pins (on the ADC)
+#define PYRO_SENSE 2
+#define VCAP_SENSE 6
+#define VBAT_SENSE 7
 
-// E22
-#define E22_CS 5
-#define E22_DI01 4
-#define E22_DI03 3
-#define E22_BUSY 6
-#define E22_RXEN 7
-#define E22_RESET 8
+// E22 (radio)
+#define E22_CS 37
+#define E22_DI01 41
+#define E22_DI03 40
+#define E22_BUSY 38
+#define E22_RXEN 39
+#define E22_RESET 6
 
 // LEDs
-#define LED_BLUE   GpioAddress(2, 014)
-#define LED_GREEN  GpioAddress(2, 015)
-#define LED_ORANGE GpioAddress(2, 016)
-#define LED_RED    GpioAddress(2, 017)
+#define LED_BLUE   8
+#define LED_GREEN  9
+#define LED_ORANGE 10
+#define LED_RED    11
 
-#define FLASH_CMD 14
-#define FLASH_CLK 18
-#define FLASH_DAT0 13
-#define FLASH_DAT1 12
-#define FLASH_DAT2 15
-#define FLASH_DAT3 16
+// FLASH memory pins
 
-#define GPIO_RESET 47
+#define FLASH_CMD 47
+#define FLASH_CLK 16
+#define FLASH_DAT0 33
+#define FLASH_DAT1 34
+#define FLASH_DAT2 18
+#define FLASH_DAT3 17
+
+//Board to Board pins
+#define B2B_EN 36
+#define B2B_READY 35
