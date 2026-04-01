1b498a93f529a6a8da833dfe5fe26e6e0c4588e8
Thomas McManamen <tm38@illinois.edu>
2026-03-31 20:15:04 -0500

    [untested] serial shell command

--- MIDAS/src/midas_shell_commands.h
+++ MIDAS/src/midas_shell_commands.h
@@ -10,6 +10,37 @@ MCommandExecutionResult set_fsm(const MShellContext& ctx) {
     return MCommandExecutionResult::OK;
 }
 
+MCommandExecutionResult hi_midas(const MShellContext& ctx) {
+    Serial.print("hi ");
+    // if no second argument, return
+    if(ctx.argc != 2) { return MCommandExecutionResult::OK; }
+    Serial.println(ctx.argv[1]);
+    return MCommandExecutionResult::OK;
+}
+
+MCommandExecutionResult serial(const MShellContext& ctx){
+    RocketSystems* arg = (RocketSystems*) ctx.sysarg;
+    if(ctx.argc != 2) { return MCommandExecutionResult::ERR_INVAL_ARGC; }
+
+    if(!strcmp(ctx.argv[1], "set")){
+        if(ctx.argc != 3) { return MCommandExecutionResult::ERR_INVAL_ARGC; }
+        uint8_t serial = atoi(ctx.argv[2]);
+        arg->eeprom.data.serial = serial;
+        arg->eeprom.commit();
+        Serial.printf("New Serial Number: %u", arg->eeprom.data.serial);
+        return MCommandExecutionResult::OK;
+    }
+    else if(!strcmp(ctx.argv[1], "get")){
+        Serial.printf("Serial Number: %u", arg->eeprom.data.serial);
+        return MCommandExecutionResult::OK;
+    }
+    else{
+        return MCommandExecutionResult::ERR_INVAL_ARGUMENT;
+    }
+}
+
 void m_shell_init_commands(MShell* sh) {
     sh->register_command("fsm", set_fsm, "fsm <state:int> - Sets the FSM state to state <state>");
+    sh->register_command("hi", hi_midas, "hi <string> - Prints hi <string>");
+    sh->register_command("serial", serial, "serial get - Get MIDAS serial number\nserial set <serialnumber:int> - Set MIDAS serial number");
 }
